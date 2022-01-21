3ce9f04279bdd288907f34d44a2f5905c51111b4
Hijiahmed <hiji.ahmed.rae.js@tuwaiq.edu.sa>
2022-01-21 16:52:06 +0300

    add

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+require("./db/db");
+
+
+
+//////
+const moviesRoute = require("./routers/Route/moviesRoute");
+const sinUpRoute = require("./routers/Route/sinUpRoute");
+const logInRoute = require("./routers/Route/logInRoute");
+const favortRoute = require("./routers/Route/favortRoute");
+app.use(moviesRoute);
+app.use(favortRoute);
+
+app.use(sinUpRoute);
+app.use(logInRoute);
+
+
+
+//////
 
 app.get("/",(req,res)=>{
     res.status(200).json("hello from my app")
@@ -12,7 +30,6 @@ app.get("/",(req,res)=>{
 
 
 
-console.log(process.env.PORT)
 app.listen(process.env.PORT, ()=>{
     console.log("server is on");
 });
